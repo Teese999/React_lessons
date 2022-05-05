2a0658321cd760c85cbcfa46fb6f702bbc6da001
Даниил Давыдов <Teese999@users.noreply.github.com>
2022-05-05 21:19:39 +0300

    COMMON

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import * as ReactDOM from 'react-dom';
+import * as React from "react";
+import { Header } from "./Header";
+
+window.addEventListener('load', () => {
+    ReactDOM.render(<Header />, document.getElementById('react_root'));
+});
